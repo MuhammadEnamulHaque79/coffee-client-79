a0f27ec83ab31f4ace4c6dade6df75bbdde2001c
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-05-14 22:55:46 +0600

    first commit

---
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>This is addcoffe page</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2>This is updated page for coffee;</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
